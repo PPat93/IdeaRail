db37720db5f9f706f8046fd9c89badd49bfc5261
PPat93 <45639693+PPat93@users.noreply.github.com>
2026-06-16 00:12:29 +0100

    v0.1 - start navbar adjustements - add tabs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,15 @@
+const navbarTabs = ["Main view", "In progress", "Postponed", "Completed", "Abandoned", "Impossible"]
+
 function Navbar() {
     return <>
         <nav className="navbar bg-body-tertiary">
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Items</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
+            <ul className="nav nav-tabs">
+
+                {navbarTabs.map((item) => (<li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">{item}</a>
+                </li>))}
+
+            </ul>
             <div className="container" id="navbar-icon">
                 <a className="navbar-brand" href="#">
                     <img src="./../../public/favicon.svg" alt="Bootstrap" width="30" height="24"/>
