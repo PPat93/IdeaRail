1f212eacbeb1f7ab69b211fa80bbf9f5f40e9a55
PPat93 <45639693+PPat93@users.noreply.github.com>
2026-06-16 22:09:26 +0100

    v0.1 add add first hook

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,27 @@
-const navbarTabs = ["Main view", "In progress", "Postponed", "Completed", "Abandoned", "Impossible"]
+import {useState} from "react";
+
+const navbarTabs = ["Main view", "In progress", "Postponed", "Completed", "Abandoned", "Impossible"];
+
 
 function Navbar() {
+
+    let [itemIndex, setItemIndex] = useState(-1);
+
+    let createTabs = (names: String[]) => {
+        return names.map((item, index: number) => (
+            <li className="nav-item">
+                <a className={(itemIndex === index ? "acitve " : "") + "nav-link "}
+                   onClick={() => setItemIndex(index)}
+                   aria-current="page"
+                   href="#">{item}
+                </a>
+            </li>));
+    }
     return <>
         <nav className="navbar bg-body-tertiary">
             <ul className="nav nav-tabs">
 
-                {navbarTabs.map((item) => (<li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">{item}</a>
-                </li>))}
+                {createTabs(navbarTabs)}
 
             </ul>
             <div className="container" id="navbar-icon">
